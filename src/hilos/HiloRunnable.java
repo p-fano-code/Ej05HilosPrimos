a694d4956e0eb6da223caa769e72742e0776eee3
Pelayo Fano <pelayofano@gmail.com>
2021-10-14 09:30:14 +0200

    Add tiempo de ejecucion

--- src/hilos/HiloRunnable.java
+++ src/hilos/HiloRunnable.java
@@ -12,7 +12,8 @@ public class HiloRunnable implements Runnable{
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		long time_start, time_end;
+		time_start = System.currentTimeMillis();
 		System.out.println("Arrancando hilo: " + Thread.currentThread().getName());
 	
 		boolean prime = true; 
@@ -23,9 +24,13 @@ public class HiloRunnable implements Runnable{
 	        }
 	    }
 	    if (prime) {
+	    	time_end = System.currentTimeMillis();
 	    	System.out.println("El numero "+numeroPrimo+" es primo");	
+	    	System.out.println("*****La ejecución ha tardado "+((time_start-time_end)/1000)+" segs*****");
 	    }  else {
+	    	time_end = System.currentTimeMillis();
 	    	System.out.println("El numero "+numeroPrimo+" NO es primo");	
+	    	System.out.println("*****La ejecución ha tardado "+((time_start-time_end)/1000)+" segs*****");
 		    	
 	    }
 	}
